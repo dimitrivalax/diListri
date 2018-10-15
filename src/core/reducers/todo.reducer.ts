7c0235409ccef918f66126260a0f46a1477bb575
dimitri mitri <dimitri.valax@in-tact.fr>
2018-10-15 16:26:47 +0200

    delete initial state

--- src/core/reducers/todo.reducer.ts
+++ src/core/reducers/todo.reducer.ts
@@ -15,47 +15,6 @@ export interface AppState {
 }
 
 const initialState = [
-  {
-    "id": "1",
-    "title": "Todo 1",
-    "content": "Something to do 1",
-    "dueDate": new Date()
-  },
-  {
-    "id": "2",
-    "title": "Todo 2",
-    "content": "Something to do 2",
-    "dueDate": new Date()
-  },
-  {
-    "id": "3",
-    "title": "Todo 3",
-    "content": "Something to do 3",
-    "dueDate": new Date()
-  },
-  {
-    "id": "4",
-    "title": "Todo 4",
-    "content": "Something to do 4"
-  },
-  {
-    "id": "5",
-    "title": "Todo 5",
-    "content": "Something to do 5",
-    "dueDate": new Date()
-  },
-  {
-    "id": "6",
-    "title": "Todo 6",
-    "content": "Something to do 6",
-    "dueDate": new Date()
-  },
-  {
-    "id": "7",
-    "title": "Todo 7",
-    "content": "Something to do 7",
-    "dueDate": new Date()
-  }
 ];
 
 export function todoReducer(state = initialState, action) {
